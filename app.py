2235fbaefc2c736f734a381a7b2efd505b31aaeb
Barry van Wyk <barry.vanwyk@gmail.com>
2026-02-21 08:40:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -981,7 +981,7 @@ header, footer, #MainMenu { visibility: hidden !important; }
 .ticker-track {
     display: flex;
     white-space: nowrap;
-    animation: tickerScroll 36s linear infinite;
+    animation: tickerScroll 15s linear infinite;
     margin-left: 140px;
 }
 
